163f59a9a5cb9d04976db821155ca6e1affd630a
dybro <casallas.17@gmail.com>
2022-06-20 19:53:57 -0500

    02-JS-I/homework/homework.js

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -78,9 +78,9 @@ function menosQueNoventa(num) {
   // Devuelve "true" si el argumento de la función "num" es menor que noventa
   // De lo contrario, devuelve "false"
   // Tu código:
-  return (num < 90); {
+  if(num < 90) {
     return true; 
-  } else {
+} else {
     return false;
   }
 }
@@ -89,7 +89,7 @@ function mayorQueCincuenta(num) {
   // Devuelve "true" si el argumento de la función "num" es mayor que cincuenta
   // De lo contrario, devuelve "false"
   // Tu código:
-  return num > 50; {
+  if num > 50 {
     return true;
   } else {
     return false;
